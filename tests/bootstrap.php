709d3abd6bfb5559cc3dd9fc5e5fd8da02522a31
Dick Olsson <dick.olsson@senzilla.com>
2014-10-30 07:54:38 +0000

    Simplified the phpunit setup

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!file_exists($file = __DIR__ . '/../vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-$loader = require $file;
-$loader->add('Doctrine\Tests\CouchDB\Replicator', __DIR__ . '/../tests');
