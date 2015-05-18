d4d2240c34f43f9026680ba7499f8471bff8c5a1
Abhishek-17 <abhi170893@gmail.com>
2015-05-18 21:35:32 +0530

    ide setup and guzzle demo

---
+++ sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhi
+ * Date: 18/5/15
+ * Time: 8:51 PM
+ */
+echo "hi all!\n";
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+echo "here1\n";
+$client = new Client();
+$response = $client->get('http://google.com/');
+$code = $response->getStatusCode();
+echo 'code is '. $code.'\n';
+$body = $response->getBody();
+var_dump($response);
+echo $body;
+echo "here3\n";
+
